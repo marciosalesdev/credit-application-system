464b58da6c742e6a6b61b957fe47bfcb35d62008
marciosalesde <marcioserrasales2014@gmail.com>
2023-10-19 16:26:22 -0300

    Implementaçao interface - CreditService(findByCustomere

--- src/main/kotlin/me/dio/credit/application/system/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/impl/CreditService.kt
@@ -20,9 +20,8 @@ class CreditService(
         return this.creditRepository.save(credit)
     }
 
-    override fun findAllByCustomer(customerId: Long): List<Credit> {
-        TODO("Not yet implemented")
-    }
+    override fun findAllByCustomer(customerId: Long): List<Credit>
+    = this.creditRepository.findAllByCustomerId(customerId)
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
        val credit: Credit = (this.creditRepository.findByCreditCode((creditCode))
--- src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -2,6 +2,7 @@ package me.dio.credit.application.system.repository
 
 import me.dio.credit.application.system.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
@@ -9,4 +10,6 @@ import java.util.*
 
 interface CreditRepository: JpaRepository<Credit, Long> {
     fun findByCreditCode(creditCode: UUID): Credit?
+@Query(value = "SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
 }
