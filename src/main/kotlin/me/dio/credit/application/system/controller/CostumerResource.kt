48d0392e61d94cea259f1f5f13ae735ce183372e
marciosalesde <marcioserrasales2014@gmail.com>
2023-10-20 16:16:21 -0300

    Endpoints CustomerResource - (findById, delete)

--- src/main/kotlin/me/dio/credit/application/system/controller/CostumerResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CostumerResource.kt
@@ -1,20 +1,28 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerVIew
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.impl.CustomerService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/customers")
 class CustomerResource (
 private val customerService: CustomerService
 ) {
-@PostMapping
-        fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
-           val savedCustomer = this.customerService.save(customerDto.toEntity())
-            return "Customer ${savedCustomer.email} saved! "
-        }
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved! "
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerVIew {
+        val customer: Customer = this.customerService.findNyId(id)
+        return CustomerVIew(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+}
